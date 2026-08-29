dc11a69e78111443a93c1ffba800334516e4f869
Yajat Yadav <yajatyadav234@gmail.com>
2026-08-29 23:44:16 +0530

    Update main.jsx

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
-const url = process.env.BACKEND_URL;
+const url = import.meta.env.BACKEND_URL;
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
